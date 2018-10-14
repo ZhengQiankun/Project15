f07053b3b855251187ece7e6dbdab4286e5ac4c9
zqk <2209847031@qq.com>
2018-10-14 23:28:31 +0800

    首页页面渲染

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,5 +1,6 @@
 from info import redis_store
 from . import index_blue
+from flask import render_template
 
 @index_blue.route('/')
 def helloworld():
@@ -24,4 +25,4 @@ def helloworld():
     # current_app.logger.warning("警告信息2")
     # current_app.logger.error("错误信息2")
 
-    return "helloworld"
+    return render_template("news/index.html")
