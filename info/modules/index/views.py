a32a113602f6c98ac95c80c41aa7453263de1fda
zqk <2209847031@qq.com>
2018-10-15 00:17:40 +0800

    captcha工具包的集成&图片验证码视图函数编写

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,12 @@
 from info import redis_store
+from info.utils.captcha.captcha import captcha
 from . import index_blue
-from flask import render_template
+from flask import render_template,current_app
+
+@index_blue.route('/image_code')
+def image_code():
+    name,text,image_data = captcha.generate_captcha()
+    return image_data
 
 @index_blue.route('/')
 def helloworld():
@@ -26,3 +32,9 @@ def helloworld():
     # current_app.logger.error("错误信息2")
 
     return render_template("news/index.html")
+#处理网站logo,浏览器在运行的时候，自动发送一个get请求，向/favicon.ico地址
+#只需要编写对应的接口,返回一张图片即可
+#解决: current_app.send_static_file,自动向static文件夹中寻找指定的资源
+@index_blue.route('/favicon.ico')
+def web_log():
+    return current_app.send_static_file("news/favicon.ico")
