4f84c92d06f18b21169aedebd23f0243ec6c90e3
zqk <2209847031@qq.com>
2018-10-16 16:00:43 +0800

    显示登录状态

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,7 @@
+from flask import session
+
 from info import redis_store
+from info.models import User
 
 from . import index_blue
 from flask import render_template,current_app
@@ -7,28 +10,24 @@ from flask import render_template,current_app
 
 @index_blue.route('/')
 def helloworld():
+    # 获取用户编号，从session
+    user_id = session.get("user_id")
 
-    # 测试redis存储数据
-    # redis_store.set("name","laowang")
-    # print(redis_store.get("name"))
+    # 判断用户是否存在
+    user  = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
 
-    # 测试session存储数据
-    # session["age"] = "13"
-    # print(session.get("age"))
+    # 将用户信息转换成字典
+    dict_data = {
+        # 如果user存在,返回左边, 否则返回右边
+        "user_info" : user.to_dict() if user else ""
+    }
 
-    # 输入记录信息，可以替代print
-    # logging.debug("调试信息1")
-    # logging.info("详细信息1")
-    # logging.warning("警告信息1")
-    # logging.error("错误信息1")
-
-    # 上面的方式可以写成，current_app来输出，区别有分割线
-    # current_app.logger.debug("调试信息2")
-    # current_app.logger.info("详细信息2")
-    # current_app.logger.warning("警告信息2")
-    # current_app.logger.error("错误信息2")
-
-    return render_template("news/index.html")
+    return render_template("news/index.html",data = dict_data)
 #处理网站logo,浏览器在运行的时候，自动发送一个get请求，向/favicon.ico地址
 #只需要编写对应的接口,返回一张图片即可
 #解决: current_app.send_static_file,自动向static文件夹中寻找指定的资源
