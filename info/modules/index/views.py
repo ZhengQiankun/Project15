c25de03a8fa394d6d985ff9b65a041d64350a619
zqk <2209847031@qq.com>
2018-10-17 19:14:22 +0800

    热门新闻展示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,8 @@
-from flask import session
+from flask import session, jsonify
 
 from info import redis_store
-from info.models import User
+from info.models import User, News
+from info.utils.response_code import RET
 
 from . import index_blue
 from flask import render_template,current_app
@@ -21,11 +22,25 @@ def helloworld():
         except Exception as e:
             current_app.logger.error(e)
 
+    # 查询数据库,根据点击量查询前十条新闻
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(10).all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="查询新闻异常")
+
+    # 将新闻对象列表, 转成字典列表
+    click_news_list = []
+    for news in news_list:
+        click_news_list.append(news.to_dict())
+
+
     # 将用户信息转换成字典
     dict_data = {
         # 如果user存在,返回左边, 否则返回右边
-        "user_info" : user.to_dict() if user else ""
-    }
+        "user_info" : user.to_dict() if user else "",
+
+        "click_news_list":click_news_list}
 
     return render_template("news/index.html",data = dict_data)
 #处理网站logo,浏览器在运行的时候，自动发送一个get请求，向/favicon.ico地址
