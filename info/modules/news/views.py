691895312bd72b11189c63fc4811755c253fe638
zqk <2209847031@qq.com>
2018-10-19 12:45:26 +0800

    收藏&取消收藏后端编写

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,8 +1,80 @@
+from flask import json
+from flask import request
+
 from info.models import News, User, Comment
 from info.utils.common import user_login_data #
 from info.utils.response_code import RET
 from . import news_blue
 from flask import render_template, current_app, jsonify, abort, session, g
+
+
+# 功能描述: 收藏/取消收藏新闻
+# 请求路径: /news/news_collect
+# 请求方式: POST
+# 请求参数:news_id,action, g.user
+# 返回值: errno,errmsg
+
+@news_blue.route('/news_collect', methods=['POST'])
+@user_login_data
+def news_collect():
+    """
+    1.判断用户是否登陆
+    2.获取参数
+    3.校验参数,为空校验
+    4.判断操作类型
+    5.根据新闻编号取出新闻对象
+    6.判断新闻对象是否存在
+    7.根据操作类型,收藏或者取消收藏操作
+    8.返回响应
+    :return:
+    """
+
+    # 1.判断用户是否登陆
+    if not g.user:
+        return jsonify(error=RET.NODATA,errmsg="用户未登录")
+
+    # 2.获取参数
+    json_data = request.data
+    dict_data = json.loads(json_data)
+    news_id = dict_data.get("news_id")
+    action = dict_data.get("action")
+
+    # 3.校验参数,为空校验
+    if not all([news_id,action]):
+        return jsonify(errno=RET.PARAMERR,errmsg="参数不全")
+
+    # 4.判断操作类型
+    if not action in ["collect","cancel_collect"]:
+        return jsonify(errno=RET.DATAERR,errmsg="操作类型有误")
+
+    # 5.根据新闻编号取出新闻对象
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="新闻获取失败")
+
+    # 6.判断新闻对象是否存在
+    if not news: return jsonify(errno=RET.NODATA,errmsg="新闻不存在")
+
+    # 7.根据操作类型,收藏或者取消收藏操作
+    try:
+        if action == "collect":
+            # 判断是否收藏过该新闻
+            if news not in g.user.collection_news:
+                g.user.collection_news.append(news)
+        else:
+            if news in g.user.collection_news:
+                g.user.collection_news.remove(news)
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="操作失败")
+
+
+    # 8.返回响应
+    return jsonify(errno=RET.OK,errmsg="操作成功")
+
 
 
 #功能描述: 获取新闻详细信息
